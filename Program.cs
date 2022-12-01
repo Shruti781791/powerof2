3934cb0eb600804bc0940df8b1a47ce0210a218d
Shruti <Kulkarnishruti@gmail.com>
2022-12-01 16:34:57 +0530

    [Shruti] -powerof2

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Powerof2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int e;
+            int result;
+
+            for (int i = 0; i < 10; i++)
+            {
+                result = 1;
+                e = i;
+
+                while (e > 0)
+                {
+                    result *= 2;
+                    e--;
+                }
+
+                Console.WriteLine("2 to the " + i + " power is " + result);
+            }
+        }
+    }
+}
+        
